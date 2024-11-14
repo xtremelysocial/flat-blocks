c5c8e075bef24bc9081f8784069c172a3c834adf
Tim Nicholson <timrnicholson@gmail.com>
2024-11-14 16:54:16 -0600

    v1.7
    Update .gitignore to include *asset.php files for future use

---
+++ assets/css/block-styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0a37270f4e697d4ac2c4');
---
+++ assets/css/wp-compat.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b0decf5fd10ca8dff70d');
