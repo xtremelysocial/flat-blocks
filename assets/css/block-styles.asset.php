084c676af64778167b4f3ec9cbe94d5cfae4c5b1
Tim Nicholson <timrnicholson@gmail.com>
2024-12-03 16:58:25 -0600

    v1.9.5
    * Fix issue with Post Comments not displaying.
    * Added new Templates for Page No Sidebar, Page No Comments or Sidebar, Post No Sidebar, Post Featured Image No Sidebar. This is so the Flat Blocks Classic child theme that uses them will retain the Template when changing from the child theme to the parent theme and back.
    * Adjusted some of the CSS for vertical margins and padding to use the global Block Gap setting (--wp--style--block-gap) rather than Spacing 40 (--wp--preset--spacing--40). By default these are the same, but if the user changes the Block Gap, the vertical spacing will be more in line. This was also to fix the theme preview on WordPress.org.
    * Featured Images on Posts and Pages with Sidebars now have a rounded border to match Featured Images in the blog.

--- assets/css/block-styles.asset.php
+++ assets/css/block-styles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '2d4a058b7a43df5b0c1e');
+<?php return array('dependencies' => array(), 'version' => '7e23fcca3f1efbfcd49c');
--- assets/css/flat-blocks.asset.php
+++ assets/css/flat-blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '45e8a1f4616f0246306b');
+<?php return array('dependencies' => array(), 'version' => '1955c9bb98393b77d252');
